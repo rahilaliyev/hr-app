30461cec2c66fa0d53db5d834e59d88417a2436b
Rahil <rahil.eliyev@gmail.com>
2024-10-29 00:26:27 +0400

    fix vite env file

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,22 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import svgr from "vite-plugin-svgr";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import svgr from 'vite-plugin-svgr';
+import eslint from 'vite-plugin-eslint2';
 
 export default defineConfig({
   plugins: [
     react(),
+    eslint(),
     svgr({
       svgrOptions: {
-        exportType: "named",
-        icon: true,
-      },
-    }),
+        exportType: 'named',
+        icon: true
+      }
+    })
   ],
   resolve: {
     alias: {
-      src: "/src",
-    },
-  },
+      src: '/src'
+    }
+  }
 });
